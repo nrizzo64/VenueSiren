751b58715283e7fb87690cb4b4e3f3706131d2b4
nrizzo64 <nicholas.rizzo64@proton.me>
2024-10-05 18:13:48 -0400

    Added client secret. Configured beginning of auth flow for spotify

--- venue-siren/server/src/app.js
+++ venue-siren/server/src/app.js
@@ -1,5 +1,8 @@
-const CLIENT_ID = ''
+require('dotenv').config();
+
+const spotifyApiKey = process.env.SPOTIFY_API_KEY;
 const express = require("express");
+const redirect_uri = 'http://localhost:5001/success';
 const app = express();
 
 // Middleware to parse JSON bodies
@@ -9,19 +12,31 @@ app.use(express.json());
 app.get("/login", (req, res) => {
   const state = generateRandomString(16);
   const scope = "user-follow-read";
-
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
+    const params = new URLSearchParams({
         response_type: "code",
-        client_id: client_id,
+        client_id: spotifyApiKey,
         scope: scope,
         redirect_uri: redirect_uri,
         state: state,
-      })
-  );
+    })
+
+  res.redirect(`https://accounts.spotify.com/authorize?${params.toString()}`);
 });
 
+app.get('/success', (req, res) => {
+    console.log("success")
+})
+
+
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 // You can add more routes here or in separate route files
 
 module.exports = app;
