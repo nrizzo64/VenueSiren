a61b0b9fed112b17536b28a520c65ae7159abdae
nrizzo64 <nicholas.rizzo64@proton.me>
2024-10-05 17:40:11 -0400

    Clean up folders and update gitignore

--- venue-siren/server/src/app.js
+++ venue-siren/server/src/app.js
@@ -1,12 +1,25 @@
-const express = require('express');
+const CLIENT_ID = ''
+const express = require("express");
 const app = express();
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
 // Example route
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+  const scope = "user-follow-read";
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+      })
+  );
 });
 
 // You can add more routes here or in separate route files
